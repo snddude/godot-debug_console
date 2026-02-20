ad532212b62f07ad00bb6cfaff73520a4251998c
Ivan <ivanbabaev217@gmail.com>
2026-02-20 13:50:52 +0800

    Added a suggestion box item limit + band aid fixes
    - Added a limit for how many suggestions can be displayed at once.
    - Suggestion box item text now gets trimmed if it's too long.
    - Suggestion box now has a minimum width (there still were and are sizing issues).

--- addons/debug_console/scripts/suggestion_box.gd
+++ addons/debug_console/scripts/suggestion_box.gd
@@ -3,10 +3,13 @@ extends Window
 
 signal item_selected(command: String)
 
+const MAX_ITEM_COUNT: int = 9
+
 @export_group("Nodes")
 @export var item_container: VBoxContainer
 
 var item_count: int = 0
+var max_item_count_reached: bool = false
 
 
 func _ready() -> void:
@@ -14,17 +17,28 @@ func _ready() -> void:
 
 
 func add_item(command: String) -> void:
+	if max_item_count_reached:
+		return
+
+	var item_text: String = ""
 	var item := Button.new()
 
-	item.text = command
+	if item_count == MAX_ITEM_COUNT:
+		item_text = "..."
+		item.disabled = true
+		max_item_count_reached = true
+	else:
+		item_text = command
+		item.pressed.connect(item_selected.emit.bind(command))
+		item.pressed.connect(hide)
+
+	item.text = item_text
 	item.alignment = HORIZONTAL_ALIGNMENT_LEFT
-	item.pressed.connect(item_selected.emit.bind(command))
-	item.pressed.connect(hide)
+	item.text_overrun_behavior = TextServer.OVERRUN_TRIM_ELLIPSIS
 
 	item_container.add_child(item)
 	item_count += 1
 
-	size.x = max(item.size.x, 96)
 	size.y = item_count * 17
 
 
@@ -33,6 +47,7 @@ func clear() -> void:
 		child.queue_free()
 
 	item_count = 0
+	max_item_count_reached = false
 
 
 func is_focused() -> bool:
